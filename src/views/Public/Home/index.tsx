3f1de387d0f56d3f2fe382ca9b2a6cf695f328dc
RaissaArantes <rarantes@inf.ufsm.br>
2021-07-05 12:38:52 -0300

    update homepage

--- src/views/Public/Home/index.tsx
+++ src/views/Public/Home/index.tsx
@@ -1,4 +1,7 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable react/jsx-no-comment-textnodes */
 import { Button } from "@chakra-ui/button";
+import "../../../css/instituicoes.css";
 import {
   Box,
   Flex,
@@ -6,42 +9,197 @@ import {
   HStack,
   SimpleGrid,
   Stack,
-  VStack,
 } from "@chakra-ui/layout";
-import { Image, Link, Text, useColorModeValue } from "@chakra-ui/react";
+import { Image, Link, Text } from "@chakra-ui/react";
 import React from "react";
 import { Link as RouterLink } from "react-router-dom";
-import logoSrc from "../../../assets/images/grayLogo.svg";
 import HeadLogo from "../../../components/Home/HeadLogo";
 import ImgLogo from "../../../components/Home/Logo";
-import WhiteTopLeaf from "../../../assets/images/whiteTopLeaf.svg";
-import WhiteBottomLeaf from "../../../assets/images/whiteBottomLeaf.svg";
 import BlueLeaf from "../../../assets/images/blueLeaf.svg";
+import { IconButton } from "@chakra-ui/react";
+import { InfoIcon } from "@chakra-ui/icons";
 
-function showCampaignDetails(){
+import Carousel from "../../../components/Home/Carousel";
+
+/*function showCampaignDetails(){
   console.log("show details");
-}
+}*/
 
 export default function Home() {
-  const titleColor = useColorModeValue("#034074", "brand.100");
-  const headingColor = useColorModeValue("#034074", "brand.200");
-  const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+  const campanhas = [
+    {
+      id: 0,
+      title: "Campanha 1",
+      picture_url: 1
+    },
+    {
+      id: 1,
+      title: "Campanha 2",
+      picture_url: 2
+    },
+    {
+      id: 2,
+      title: "Campanha 3",
+      picture_url: 3
+    },
+    {
+      id: 3,
+      title: "Campanha 4",
+      picture_url: 4
+    },
+    {
+      id: 4,
+      title: "Campanha 5",
+      picture_url: 5
+    },
+    {
+      id: 5,
+      title: "Campanha 6",
+      picture_url: 6
+    },
+    {
+      id: 6,
+      title: "Campanha 7",
+      picture_url: 7
+    },
+    {
+      id: 7,
+      title: "Campanha 8",
+      picture_url: 8
+    },
+    {
+      id: 8,
+      title: "Campanha 9",
+      picture_url: 9
+    },
+    {
+      id: 9,
+      title: "Campanha 10",
+      picture_url: 10
+    },
+    {
+      id: 10,
+      title: "Campanha 11",
+      picture_url: 11
+    },
+    {
+      id: 11,
+      title: "Campanha 12",
+      picture_url: 12
+    },
+    {
+      id: 12,
+      title: "Campanha 13",
+      picture_url: 13
+    },
+    {
+      id: 13,
+      title: "Campanha 14",
+      picture_url: 14
+    },
+    {
+      id: 14,
+      title: "Campanha 15",
+      picture_url: 15
+    }
+  ]
+  const institutes = [
+    {
+      id: 0,
+      name: "Instituição 1",
+      picture_url: 1
+    },
+    {
+      id: 1,
+      name: "Instituição 2",
+      picture_url: 2
+    },
+    {
+      id: 2,
+      name: "Instituição 3",
+      picture_url: 3
+    },
+    {
+      id: 3,
+      name: "Instituição 4",
+      picture_url: 4
+    },
+    {
+      id: 4,
+      name: "Instituição 5",
+      picture_url: 5
+    },
+    {
+      id: 5,
+      name: "Instituição 6",
+      picture_url: 6
+    },
+    {
+      id: 6,
+      name: "Instituição 7",
+      picture_url: 7
+    },
+    {
+      id: 7,
+      name: "Instituição 8",
+      picture_url: 8
+    },
+    {
+      id: 8,
+      name: "Instituição 9",
+      picture_url: 9
+    },
+    {
+      id: 9,
+      name: "Instituição 10",
+      picture_url: 10
+    },
+    {
+      id: 10,
+      name: "Instituição 11",
+      picture_url: 11
+    },
+    {
+      id: 11,
+      name: "Instituição 12",
+      picture_url: 12
+    },
+    {
+      id: 12,
+      name: "Instituição 13",
+      picture_url: 13
+    },
+    {
+      id: 13,
+      name: "Instituição 14",
+      picture_url: 14
+    },
+    {
+      id: 14,
+      name: "Instituição 15",
+      picture_url: 15
+    }
+  ]
   return (
-    <Box backgroundColor="gray.200">
+    <Box 
+      backgroundColor="gray.200"
+    >
       <Box
         as="header"
         boxShadow="5px 5px 10px rgba(0, 0, 0, 0.3)"
-        backgroundColor={headingColor}
+        backgroundColor="bluish.200"
       >
+      <div>
         <Flex
           maxW="1400px"
-          paddingX={{ base: "35px", sm: "2px" }}
+          paddingX={{ base: "35px", sm: "50px" }}
           justify="space-between"
           align="center"
           wrap="wrap"
           margin="0 auto"
+          pb={2}
         >
-          <HeadLogo height="10rem" width="12rem"/>
+          <HeadLogo height="5rem" width="7rem"/>
           <div />
           <Stack
             spacing={{ base: 2, sm: 10 }}
@@ -50,7 +208,7 @@ export default function Home() {
           >
             <Heading
               color="white"
-              size="lg"
+              size="md"
               cursor="pointer"
               href="#campanhas"
               as={Link}
@@ -60,7 +218,7 @@ export default function Home() {
 
             <Heading
               color="white"
-              size="lg"
+              size="md"
               cursor="pointer"
               href="#instituicoes"
               as={Link}
@@ -70,7 +228,7 @@ export default function Home() {
 
             <Heading
               color="white"
-              size="lg"
+              size="md"
               cursor="pointer"
               href="#sobre"
               as={Link}
@@ -79,53 +237,55 @@ export default function Home() {
             </Heading>
           </Stack>
 
-          <HStack spacing={5} margin={{ base: "15px auto", lg: "initial" }}>
+          <HStack spacing={5} margin={{ lg: "initial" }} pt={2}>
             <Button
               bgColor="white"
-              color={headingColor}
-              borderRadius={50}
+              color="bluish.400"
+              borderRadius={10}
               p={3}
               pl={6}
               pr={6}
               as={RouterLink}
               to="/login"
-              size="1xl"        
+              size="sm"        
               boxShadow="0px 6px 20px rgba(0, 0, 0, 0.3)"
             >
               Entrar
             </Button>
             <Button
               bgColor="white"
-              color={headingColor}
-              borderRadius={50}
+              color="bluish.400"
+              borderRadius={10}
               p={3}
               as={RouterLink}
               to="/login"
-              size="1xl"        
+              size="sm"        
               boxShadow="0px 6px 20px rgba(0, 0, 0, 0.3)"
             >
               Cadastrar
             </Button>
           </HStack>
         </Flex>
+      </div>
       </Box> 
       <Flex
         maxW="1400px"
-        paddingX={{ base: "35px", sm: "2px" }}
+        paddingX={{ base: "30px", sm: "50px" }}
         paddingY={2}
+        pb={10}
         justify="space-between"
         align="center"
         wrap="wrap"
         margin="0 auto"
       >
-        <ImgLogo type='blue' />
-        <Heading color={titleColor} size="2xl" textAlign="center">
+        <ImgLogo type='bluish' />
+        <Heading color="bluish.200" size="2xl" textAlign="center">
           BEM-VINDO AO FAZ UM <br /> BEM!
           <br />
           <Box 
             as="button" 
-            borderRadius={50} 
-            bg={headingColor} 
+            borderRadius={16} 
+            bg="bluish.100" 
             color="white"
             mt={10}
             p={4}
@@ -133,6 +293,10 @@ export default function Home() {
             pr={10}
             width="max-content"
             boxShadow="0px 8px 10px rgba(0, 0, 0, 0.3)"
+            _hover={{
+              background: "bluish.200",
+              transition: ".5s"
+            }}
           >
             <Text
               fontSize="2xl"
@@ -146,108 +310,17 @@ export default function Home() {
         </Heading>
       </Flex>
       <Box
-        backgroundColor="brand.500"
+        backgroundColor="bluish.200"
         width="100%"
         p={2}
         pt={20}
         pb={20}
         textAlign="center"
       >
-        <SimpleGrid
-          minChildWidth={{ base: "100%", md: "40%", lg: "300px" }}
-          spacing={10}
-          paddingX={{ base: 4, md: 6, lg: 10 }}
-          paddingY={4}
-          maxW="1600px"
-          margin="0 auto"
-        >
-          <VStack
-            paddingY={3}
-          >
-            <Image
-              height="100px"
-              objectFit="contain"
-              src={WhiteTopLeaf}
-              alt="white icon top"
-            />
-            <Heading color="white" size="lg" textAlign="center">
-              CAMPANHAS EM ANDAMENTO
-            </Heading>
-            <Image
-              height="100px"
-              objectFit="contain"
-              src={WhiteBottomLeaf}
-              alt="white icon bottom"
-            />
-          </VStack>
-          {items.map(
-            (value) => (
-              <VStack
-                spacing={2}
-                align="center"
-                paddingX={1}
-                paddingY={1}
-                borderRadius={15}
-                pb={8}
-                backgroundColor="gray.100"
-                cursor="pointer"
-                title="Clique para ver mais infomações sobre a campanha"
-                boxShadow="5px 5px 10px rgba(0, 0, 0, 0.3)"
-                onMouseEnter={showCampaignDetails}
-                _hover={{
-                  marginTop: "-15px",
-                  marginBottom: "15px"
-                }}
-              >
-                <Image
-                  src={`https://fazumbem.inf.ufsm.br/images/entidades/${value}.png`}
-                  fallbackSrc={logoSrc}
-                  alt="logo"
-                  height="260px"
-                  width="280px"
-                  objectFit="fill"
-                  borderRadius={15}
-                  filter="none"
-                  mb={2}
-                  mt={4}
-                  _hover={{
-                    filter: "brightness(30%)",
-                    color: "white"
-                  }}
-                />
-                <Text
-                  fontSize="lg"
-                  color={titleColor}
-                  fontWeight="bold"
-                  textAlign="left"
-                >
-                  Nome da Campanha {value} 
-                </Text>
-              </VStack>
-            )
-          )}
-        </SimpleGrid>
-        <Button
-          bgColor="white"
-          color={headingColor}
-          borderRadius={50}
-          p={4}
-          pl={10}
-          pr={10}
-          mt={10}
-          as={RouterLink}
-          to="/login"
-          size="3xl"        
-          boxShadow="5px 5px 10px rgba(0, 0, 0, 0.3)"
-          >
-          <Text
-            fontSize="2xl"
-            color={headingColor}
-            fontWeight="medium"
-          >
-            Ver todas
-          </Text>
-        </Button>
+        <div>
+          <h1 className="slider_title">CAMPANHAS EM ANDAMENTO</h1>
+          <Carousel campanhas={campanhas}/>
+        </div>
       </Box>
       <Box
         width="max-content"
@@ -274,17 +347,19 @@ export default function Home() {
               height="100px"
               objectFit="contain"
               src={BlueLeaf}
-              alt="blue icon start"
+              alt="bluish icon start"
               transform="rotateY(180deg)"
+              filter="brightness(180%)"
             />
-            <Heading color="brand.500" size="2xl" textAlign="center">
+            <Heading color="bluish.200" size="2xl" textAlign="center">
               INSTITUIÇÕES <br />PARTICIPANTES
             </Heading>
             <Image
               height="100px"
               objectFit="contain"
               src={BlueLeaf}
-              alt="blue icon end"
+              alt="bluish icon end"
+              filter="brightness(200%)"
             />
           </Stack>
         </Flex>
@@ -297,38 +372,33 @@ export default function Home() {
         maxW="1600px"
         margin="0 auto"
       >
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((value) => (
-          <VStack
-            spacing={1}
-            align="center"
-            justify="center"  
-            padding={3}
-            borderRadius={15}
-            backgroundColor="brand.500"
-            cursor="pointer"
-            boxShadow="5px 10px 10px rgba(0, 0, 0, 0.3)"
-            _hover={{
-              marginTop: "-15px",
-              marginBottom: "15px"
-            }}
-          >
-            <Image
-              src={`https://fazumbem.inf.ufsm.br/images/logos/${value}.png`}
-              fallbackSrc={logoSrc}
-              alt="logo"
-              height="100px"
-              objectFit="fill"
-              borderRadius={10}
-            />
-            <Text
-              fontSize="1xl"
-              color="white"
-              fontWeight="bold"
-              textAlign="center"
-            >
-              Instituição Nome {value}
-            </Text>
-          </VStack>
+        {institutes.map((institute: any = []) => (
+          <div key={institute.id}>
+            <div className="institute-card">
+              <div className="institute-card-image">
+                <Image src={`https://fazumbem.inf.ufsm.br/images/logos/${institute.picture_url}.png`}/>
+              </div>
+              <ul className="institute-icons">
+                <li>
+                  <a href="/">
+                    <i>
+                      <IconButton 
+                        aria-label="info" 
+                        icon={<InfoIcon />}  
+                        _hover={{ 
+                          bgColor: "bluish.400", 
+                          transition: "0.2s"
+                        }} 
+                      />
+                    </i>
+                  </a>
+                </li>
+              </ul>
+              <div className="institute-details">
+                <h2>{ institute.name }</h2>
+              </div>
+            </div>
+          </div>
         ))}
       </SimpleGrid>
       <Box
@@ -339,7 +409,7 @@ export default function Home() {
       >
         <Flex
           maxW="1400px"
-          paddingX={{ base: "35px", sm: "2px" }}
+          paddingX={{ base: "35px", sm: "55px" }}
           paddingY={2}
           justify="space-between"
           align="center"
@@ -347,8 +417,7 @@ export default function Home() {
           margin="0 auto"
         >
           <ImgLogo type='white' />
-
-          <Heading color="white" size="2xl" textAlign="center">
+          <Heading color="white" size="2xl" mr={10} textAlign="center">
             Atalhos
             <Text
               fontSize="2xl"
@@ -381,6 +450,7 @@ export default function Home() {
           </Heading>
         </Flex>
       </Box>
+
     </Box>
   );
 }
