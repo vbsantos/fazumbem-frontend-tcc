1f9e62046e8a38dd0605ca43c16b28287e631fae
pizzutta <pizzutti.v@gmail.com>
2021-08-19 13:19:26 -0300

    Adaptação para novo layout. (Contact)

--- src/views/Public/Contacts/index.tsx
+++ src/views/Public/Contacts/index.tsx
@@ -1,123 +1,134 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable react/jsx-no-comment-textnodes */
 import "../../../css/instituicoes.css";
-import { Box, Flex, Heading } from "@chakra-ui/layout";
-import { 
-  Text, 
-  Center, 
-  Tooltip,
-  Link,
-  Divider
+import { Box, Heading } from "@chakra-ui/layout";
+import {
+  Text,
+  Input,
+  Textarea,
+  useMediaQuery
 } from "@chakra-ui/react";
 import Header from "../../../components/PublicHeader";
 import Footer from "../../../components/Footer";
-import {
-  FaFacebook,
-  FaInstagram,
-  FaRegEnvelope,
-  FaYoutube,
-  FaMapMarkerAlt,
-  FaPhone
-} from "react-icons/fa";
 
 export default function Home() {
+  const [isMobile] = useMediaQuery("(max-width: 576px)")
+  const [isDesktop] = useMediaQuery("(min-width: 769px)")
+  const [isGreater] = useMediaQuery("(min-width: 1200px)")
   return (
     <Box backgroundColor="gray.200" >
       <Header />
-      <Heading
-        color="bluish.100"
-        size="2xl"
+      <Box
+        padding={!isDesktop ? "50px 5px" : "50px"}
         textAlign="center"
-        mt="5rem"
-        mb="2rem"
       >
-        CONTATOS
-      </Heading>
-      <Text color="bluish.100" fontSize="1.4rem" m={10} textAlign="center">
-        Entre em contato com a equipe organizadora e idealizadora do projeto
-        através do e-mail de contato, telefone ou presencialmente. Não deixe
-        de seguir o projeto nas redes sociais para se manter atualizado!
-      </Text>
-      <Box 
-        p={1} 
-        pb={20} 
-        m={10}
-        id="about"
-      >
-        <Box>
-          <Box fontSize="1.2rem" color="bluish.100">
-            <div style={{ marginBottom: "-30px" }}>
-              <FaRegEnvelope />
-            </div>
-            <Text ml={6} fontWeight={500}>E-mail</Text><Text>e_mail@mail.com</Text>
-            <br />
-            <div style={{ marginBottom: "-30px" }}>
-              <FaMapMarkerAlt />
-            </div>
-            <Text ml={6} fontWeight={500}>Endereço</Text><Text>
-              Av. Roraima nº 1000 Cidade Universitária Bairro - Camobi, Santa
-              Maria - RS, 97105-900
-            </Text>
-            <br />
-            <div style={{ marginBottom: "-30px" }}>
-              <FaPhone />
-            </div>
-            <Text ml={6} fontWeight={500}>Telefone</Text><Text>
-              (55) XXXXX-XXXX
-            </Text>
-          </Box>
-          <Divider borderBottom="1px solid #034074" pt={10} mb={10} />
-          <Box textAlign="center">
-            <Heading size="2xl" mb={2} color="brand.300">
-              REDES SOCIAIS
-            </Heading>
-            <Text color="bluish.100" fontSize="1.4rem" m={10} textAlign="center">
-              Confira abaixo as redes sociais do projeto. Não deixe
-              de seguir nas redes sociais para se manter atualizado!
-            </Text>
-            <Center>
-              <Flex flex-direction="row">
-                <Tooltip 
-                  hasArrow 
-                  label="Facebook"
-                  bg="bluish.200" 
+        <Heading>
+          <Text
+            display="inline"
+            fontSize={isMobile ? "35px" : "55px"}
+            fontFamily="Comfortaa"
+            color="white"
+            backgroundColor="#ED6A5A"
+          >
+            &nbsp;Contato&nbsp;
+          </Text>
+        </Heading>
+        <Box marginTop="50px;">
+          <Text
+            fontSize={isMobile ? "18px" : "26px"}
+            color="bluish.100"
+            lineHeight="31.69px"
+            padding={isGreater ? "0 24%" : "0 5px"}
+          >
+            Possui dúvidas, sugestões ou mensagens para compartilhar?
+            Abaixo você pode entrar em contato com a plataforma Faz um Bem!
+          </Text>
+        </Box>
+        <Box
+          marginTop="50px;"
+          padding={isGreater ? "0 25%" : "0 5px"}
+
+        >
+          <Box
+            backgroundColor="white"
+            borderRadius="50px"
+            color="bluish.100"
+            padding="25px 50px"
+            boxShadow="2px 4px 9px rgba(0, 0, 0, 0.25)"
+          >
+            <form>
+              <Text fontWeight="700" fontSize={isMobile ? "20px" : "22px"}>
+                Email
+              </Text>
+              <Input
+                focusBorderColor="bluish.100"
+                backgroundColor="#F7F6F3"
+                borderRadius="50px"
+                textAlign="center"
+                border="none"
+                margin="15px 0 50px"
+                type="email"
+              />
+              <Text fontWeight="700" fontSize={isMobile ? "20px" : "22px"}>
+                Seu nome
+              </Text>
+              <Input
+                focusBorderColor="bluish.100"
+                backgroundColor="#F7F6F3"
+                borderRadius="50px"
+                textAlign="center"
+                border="none"
+                margin="15px 0 50px"
+                type="text"
+              />
+              <Text fontWeight="700" fontSize={isMobile ? "20px" : "22px"}>
+                Assunto
+              </Text>
+              <Input
+                focusBorderColor="bluish.100"
+                backgroundColor="#F7F6F3"
+                borderRadius="50px"
+                textAlign="center"
+                border="none"
+                margin="15px 0 50px"
+                type="text"
+              />
+              <Text fontWeight="700" fontSize={isMobile ? "20px" : "22px"}>
+                Mensagem
+              </Text>
+              <Textarea
+                focusBorderColor="bluish.100"
+                backgroundColor="#F7F6F3"
+                borderRadius="25px"
+                minHeight="200px"
+                display="block"
+                border="none"
+                margin="15px 0 50px"
+                resize="none"
+              />
+              <Box
+                borderRadius="50px"
+                bg="#ED6A5A"
+                padding="7px 46px"
+                boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
+                as="button"
+                type="submit"
+                _hover={{
+                  textDecoration: "none",
+                  background: "#F18C7E",
+                  transition: ".5s",
+                }}
+              >
+                <Text
+                  fontSize={isMobile ? "22px" : "24px"}
                   color="white"
-                  placement="top"
-                  borderRadius="8px"
-                  transition="0.2s"
+                  fontWeight="500"
+                  textAlign="center"
                 >
-                  <Box m={2} as={Link} href="/" color="bluish.100">
-                    <FaFacebook  size={50} />
-                  </Box>
-                </Tooltip>
-                <Tooltip 
-                  hasArrow 
-                  label="Instagram"
-                  bg="bluish.200" 
-                  color="white"
-                  placement="top"
-                  borderRadius="8px"
-                  transition="0.2s"
-                >
-                  <Box m={2} as={Link} href="/" color="bluish.100">
-                    <FaInstagram size={50} />
-                  </Box>
-                  </Tooltip>
-                <Tooltip 
-                  hasArrow 
-                  label="Youtube"
-                  bg="bluish.200" 
-                  color="white"
-                  placement="top"
-                  borderRadius="8px"
-                  transition="0.2s"
-                >
-                  <Box m={2} as={Link} href="/" color="bluish.100">
-                    <FaYoutube size={50} />
-                  </Box>
-                </Tooltip>
-              </Flex>
-            </Center>
+                  Enviar
+                </Text>
+              </Box>
+            </form>
           </Box>
         </Box>
       </Box>
